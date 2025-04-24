8d3c270723ca3b6d062a64a4cd7b26e840336f28
MAHDTech <MAHDTech@saltlabs.tech>
2025-04-24 11:37:43 +1000

    fix: linters (#14)

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -9,7 +9,7 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/go-playground/validator/v10"
+	validator "github.com/go-playground/validator/v10"
 	yaml "gopkg.in/yaml.v3"
 
 	utils "github.com/MAHDTech/nixos-installer/pkg/utils"
@@ -190,7 +190,10 @@ func validateConfig(configData *Config) error {
 
 	// ZFS ashift validation.
 	if configData.ZFS.Ashift < 9 || configData.ZFS.Ashift > 16 {
-		return fmt.Errorf("invalid ashift value: %d. Must be between 9 and 16.", configData.ZFS.Ashift)
+		return fmt.Errorf(
+			"invalid ashift value: %d. Must be between 9 and 16",
+			configData.ZFS.Ashift,
+		)
 	}
 
 	// If there is more than disk, are we mirroring or striping the boot and root pools?
